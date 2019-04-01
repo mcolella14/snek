8a32a460d2099088be43c5c07fd5b4cb13a8d879
Matthew Colella <mcolella14@gmail.com>
2019-04-01 15:16:26 -0400

    not really like snake anymore...

--- game.js
+++ game.js
@@ -9,9 +9,11 @@ let downPressed;
 let score = 0;
 let goalXPos = 100;
 let goalYPos = 100;
-let obstacleXPos;
-let obstacleYPos;
+let obstacleXPos = 400;
+let obstacleYPos = 470;
 let highScore = 0;
+let obsMoveRight = true;
+let obsMoveDown = true;
 
 document.getElementById('reset').addEventListener('click', () =>{
  xPos = 250;
@@ -36,34 +38,34 @@ function draw() {
       ctx.shadowColor = "white";
       ctx.arc(xPos, yPos, 5, 0, Math.PI * 2, true,);
       ctx.arc(goalXPos, goalYPos, 10, 0, Math.PI * 2, true,); 
-      ctx.rect(obstacleXPos, obstacleYPos, 10*score, 10*score);
-      if(leftPressed == true && upPressed == true){
+      ctx.rect(obstacleXPos, obstacleYPos, 30, 30);
+      if(leftPressed == true && upPressed == true && xPos >= 10 && yPos >= 10){
         xPos-=5;
         yPos-=5;
       }
-      else if(rightPressed == true && upPressed == true){
+      else if(rightPressed == true && upPressed == true && xPos <= 690 && yPos >= 10){
         xPos+=5;
         yPos-=5;
       }
-      else if(leftPressed == true && downPressed == true){
+      else if(leftPressed == true && downPressed == true && xPos >= 10 && yPos <= 690){
         xPos-=5;
         yPos+=5;
       }
-      else if(rightPressed == true && downPressed == true){
+      else if(rightPressed == true && downPressed == true && xPos <= 690 && yPos <= 690){
         xPos+=5;
         yPos+=5;
       }
 
-      else if(leftPressed == true){
+      else if(leftPressed == true && xPos >= 10){
         xPos-=10;
       }
-      else if(rightPressed == true){
+      else if(rightPressed == true && xPos <= 690){
         xPos+=10;
       }
-      else if(upPressed == true){
+      else if(upPressed == true && yPos >= 10){
         yPos-=10;
       }
-      else if(downPressed == true){
+      else if(downPressed == true && yPos <= 690){
         yPos+=10;
       }
 
@@ -71,24 +73,48 @@ function draw() {
         goalXPos = Math.random()*700;
         goalYPos = Math.random()*700;
         score++;
-        obstacleXPos = Math.random()*650;
-        obstacleYPos = Math.random()*650; 
+       
         
       }
 
-      if ((xPos > obstacleXPos && xPos < obstacleXPos + 10*score) && (yPos > obstacleYPos && yPos < obstacleYPos + 10*score)){
+      if ((xPos > obstacleXPos && xPos < obstacleXPos + 30) && (yPos > obstacleYPos && yPos < obstacleYPos + 30)){
         ctx.clearRect(0, 0, 1000, 1000); // clear canvas
         xPos = 250;
         yPos = 250;
         score = 0;
         goalXPos = 100;
         goalYPos = 100;
-        obstacleXPos = undefined;
-        obstacleYPos = undefined; 
+        obstacleXPos = 400;
+        obstacleYPos = 470; 
         
 
       }
+      if (obstacleXPos > 670){
+        obsMoveRight = false;
+      }
+      if (obstacleXPos < 0){
+        obsMoveRight = true;
+      }
+      if (obstacleYPos > 670){
+        obsMoveDown = false;
+      }
+      if (obstacleYPos < 0){
+        obsMoveDown = true;
+      }
 
+      if (obsMoveRight){
+        obstacleXPos += ((score + Math.random())*2)
+      }
+      if(obsMoveRight == false) {
+        obstacleXPos -= ((score + Math.random())*2)
+      }  
+      if (obsMoveDown){
+        obstacleYPos += (score + Math.random())
+      }
+      if (obsMoveDown == false){
+      obstacleYPos -= (score + Math.random())
+      }
+    
       if(score >= highScore){
         highScore = score;
       }
@@ -118,6 +144,7 @@ function draw() {
     else if(e.key == "Down" || e.key == "ArrowDown") {
         downPressed = true;
 }
+  
 }
 
 function keyUpHandler(e) {
